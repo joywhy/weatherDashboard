bfa821a3645c247ceb78ad42c83ece3261327d15
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-11-14 00:58:20 +0900

    fix: Hourly 퍼블리싱 수정

--- src/components/home/Hourly.tsx
+++ src/components/home/Hourly.tsx
@@ -1,34 +1,34 @@
 import { Widget } from '../common/Widget';
 import { Card } from '@/components/ui';
 function Hourly() {
-  /**
-   * section 과 article
-   *     section 은 좀더 포괄적으로 주제별 영역을 나누는 역할이고   article 은 좀더 구체적으로 블로그 글 포럼 뉴스 기사등을 나누는 역할이라고
-   *    생각해서  오늘의 날씨를 today 는 article로 감쌌습니다.
-   */
-
   const data = [
     { time: '오전0시', weather: 17, isSunset: false },
     { time: '오전1시', weather: 16, isSunset: false },
     { time: '오전2시', weather: 16, isSunset: false },
     { time: '오전3시', weather: 15, isSunset: false },
     { time: '오전4시', weather: 17, isSunset: false },
+    { time: '오전4시', weather: 17, isSunset: false },
+    { time: '오전4시', weather: 17, isSunset: false },
+    { time: '오전4시', weather: 17, isSunset: false },
+    { time: '오전4시', weather: 17, isSunset: false },
   ];
   return (
     <Widget
       title="Hourly"
       des="오늘의 시간대별 날씨를 조회하고 있습니다."
-      size="col-span-2"
+      size="col-span-2 relative"
     >
-      <div className="flex gap-3">
-        {data.map((item, index) => (
-          <HourlyCard
-            key={index}
-            time={item.time}
-            weather={item.weather}
-            isSunset={item.isSunset}
-          />
-        ))}
+      <div className="flex gap-3  absolute w-[calc(100%-30px)] overflow-hidden">
+        <div className="  flex  gap-3 ">
+          {data.map((item, index) => (
+            <HourlyCard
+              key={index}
+              time={item.time}
+              weather={item.weather}
+              isSunset={item.isSunset}
+            />
+          ))}
+        </div>
       </div>
     </Widget>
   );
@@ -43,7 +43,7 @@ function HourlyCard({
   isSunset: boolean;
 }) {
   return (
-    <Card className="bg-[--gray-color-200] p-2 h-[125px] w-[97px] flex flex-col items-center justify-between">
+    <Card className="bg-[--gray-color-200] p-2 h-[125px] w-[97px]  flex flex-col items-center justify-between">
       <p className="text-[--gray-color-300]">{time}</p>
       {isSunset ? (
         <img src="src/assets/icons/1000d.svg" alt="sunset" className="w-10" />
--- src/components/home/Today.tsx
+++ src/components/home/Today.tsx
@@ -1,11 +1,6 @@
 import { Widget } from '../common/Widget';
 import { Separator } from '@/components/ui';
 function Today() {
-  /**
-   * section 과 article
-   *     section 은 좀더 포괄적으로 주제별 영역을 나누는 역할이고   article 은 좀더 구체적으로 블로그 글 포럼 뉴스 기사등을 나누는 역할이라고
-   *    생각해서  오늘의 날씨를 today 는 article로 감쌌습니다.
-   */
   const data = {
     temp: 19,
     weather: 'sunny',
